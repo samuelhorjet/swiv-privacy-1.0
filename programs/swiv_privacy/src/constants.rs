9c7f579da475ff0af5cb2f6ec4764acc46b0c5d4
David-Horjet <davidojetola956@gmail.com>
2026-01-28 03:27:41 +0100

    refactor: rename protocol seed

--- programs/swiv_privacy/src/constants.rs
+++ programs/swiv_privacy/src/constants.rs
@@ -1,8 +1,7 @@
-pub const SEED_PROTOCOL: &[u8] = b"global_config_v1";
+pub const SEED_PROTOCOL: &[u8] = b"protocol";
 pub const SEED_POOL: &[u8] = b"pool";
 pub const SEED_POOL_VAULT: &[u8] = b"pool_vault";
 pub const SEED_BET: &[u8] = b"user_bet";
-pub const SEED_POOL_SECRETS: &[u8] = b"pool_secrets";
 
 pub const MAX_STRATEGY_LENGTH: usize = 32;
 pub const MERCY_BUFFER_DEFAULT: u64 = 500; 
